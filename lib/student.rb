95466e8c71f8e287fecedd02698bb7c917996bbd
cpan001 <github email address>
2017-09-01 12:05:02 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+require 'pry'
+
+class Student < User
+
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge_str)
+    self.knowledge << knowledge_str
+  end
 
 end
+
+
+# binding.pry
+# ""
